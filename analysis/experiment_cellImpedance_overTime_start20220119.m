89eee2ab6facbea82bb10e6149770d7dfd41d1c0
Hunter Strathman <hstrathman@gmail.com>
2022-01-20 17:33:41 -0700

    Add Day 02 data.

--- analysis/experiment_cellImpedance_overTime_start20220119.m
+++ analysis/experiment_cellImpedance_overTime_start20220119.m
@@ -22,9 +22,9 @@ outputDir = ['../output/' parts{end}];
 % Names should be good to go. Just uncomment here and below to add in each
 % day.
 [gamryStructure_EIS_cell_Day01] = ...
-    extractImpedanceDataGlobal('..\rawData\Gamry\20220120_WPI02_GelCell');
-% [gamryStructure_EIS_cell_Day02] = ...
-%     extractImpedanceDataGlobal('..\rawData\Gamry\20211208_WPI02_InVitro_GelCM\EIS');
+    extractImpedanceDataGlobal('..\rawData\Gamry\20220120_WPI02_GelCell_Day01');
+[gamryStructure_EIS_cell_Day02] = ...
+    extractImpedanceDataGlobal('..\rawData\Gamry\20220121_WPI02_GelCell_Day02');
 % [gamryStructure_EIS_cell_Day03] = ...
 %     extractImpedanceDataGlobal('..\rawData\Gamry\20211221_WPI02_InVitro_GelCM\EIS');
 
@@ -186,6 +186,29 @@ title('Day 01 - Individual Measurements')
 % with it, the measured impedance. I'm leaving it out of the average plot
 % for now. 
 
+% Day 02 %
+yyaxis left
+for ii = 1:length(gamryStructure_EIS_cell_Day02)
+    loglog( gamryStructure_EIS_cell_Day02(ii).f, ...
+            gamryStructure_EIS_cell_Day02(ii).Zmag )
+    hold on
+end
+xlabel('Frequency (Hz)')
+ylabel('mag(Impedance) (Ohm)')
+
+yyaxis right
+for ii = 1:length(gamryStructure_EIS_cell_Day02)
+    loglog( gamryStructure_EIS_cell_Day02(ii).f, ...
+            gamryStructure_EIS_cell_Day02(ii).Phase )
+    hold on
+end
+set(gca, 'XScale', 'log')
+xlabel('Frequency (Hz)')
+ylabel('Phase')
+xlim([9.9 1e6])
+legend('CM-r1', 'CM-r2', 'Gel 01', 'Gel 02', 'Gel 03', 'Gel 04')
+title('Day 01 - Individual Measurements')
+
 %% Plot Averages
 
 % CELL Day 1% 
@@ -215,7 +238,7 @@ figure
 yyaxis left
 errorbar( gamryStructure_EIS_cell_Day01(1).f, ...
         meanMeanArray, ...
-        stdMeanArray, 'LineWidth', 2.0)
+        stdMeanArray, 'LineWidth', 1.0)
 hold on
 set(gca, 'YScale', 'log')
 set(gca, 'XScale', 'log')
@@ -225,52 +248,52 @@ ylabel('mag(Impedance) (Ohm)')
 yyaxis right
 errorbar( gamryStructure_EIS_cell_Day01(1).f, ...
         meanPhaseArray, ...
-        stdPhaseArray, 'LineWidth', 2.0)
+        stdPhaseArray, 'LineWidth', 1.0)
 hold on
 set(gca, 'XScale', 'log')
 xlabel('Frequency (Hz)')
 ylabel('Phase')
 
-% % CELL Day 2 % 
-% meanArray = [];
-% PhaseArray = [];
-% meanMeanArray = [];
-% stdMeanArray = [];
-% meanPhaseArray = [];
-% stdPhaseArray = [];
-% 
-% % 01/20
-% pointerArray = [3 4 5 6]; 
-% numMeasures = length(pointerArray);
-% for ii = 1:numMeasures
-%     jj = pointerArray(ii);
-%     meanArray = [meanArray gamryStructure_EIS_cell_Day02(jj).Zmag];
-%     PhaseArray = [PhaseArray gamryStructure_EIS_cell_Day02(jj).Phase];
-% end
-% 
-% meanMeanArray = mean(meanArray, 2);
-% stdMeanArray = std(meanArray, 0, 2);
-% meanPhaseArray = mean(PhaseArray, 2);
-% stdPhaseArray = std(PhaseArray, 0, 2);
-% 
-% yyaxis left
-% errorbar( gamryStructure_EIS_cell_Day02(1).f, ...
-%         meanMeanArray, ...
-%         stdMeanArray, 'LineWidth', 2.0)
-% hold on
-% set(gca, 'YScale', 'log')
-% set(gca, 'XScale', 'log')
-% xlabel('Frequency (Hz)')
-% ylabel('mag(Impedance) (Ohm)')
-% 
-% yyaxis right
-% errorbar( gamryStructure_EIS_cell_Day03(1).f, ...
-%         meanPhaseArray, ...
-%         stdPhaseArray, 'LineWidth', 2.0)
-% hold on
-% set(gca, 'XScale', 'log')
-% xlabel('Frequency (Hz)')
-% ylabel('Phase')
+% CELL Day 2 % 
+meanArray = [];
+PhaseArray = [];
+meanMeanArray = [];
+stdMeanArray = [];
+meanPhaseArray = [];
+stdPhaseArray = [];
+
+% 01/20
+pointerArray = [3 4 5 6]; 
+numMeasures = length(pointerArray);
+for ii = 1:numMeasures
+    jj = pointerArray(ii);
+    meanArray = [meanArray gamryStructure_EIS_cell_Day02(jj).Zmag];
+    PhaseArray = [PhaseArray gamryStructure_EIS_cell_Day02(jj).Phase];
+end
+
+meanMeanArray = mean(meanArray, 2);
+stdMeanArray = std(meanArray, 0, 2);
+meanPhaseArray = mean(PhaseArray, 2);
+stdPhaseArray = std(PhaseArray, 0, 2);
+
+yyaxis left
+errorbar( gamryStructure_EIS_cell_Day02(1).f, ...
+        meanMeanArray, ...
+        stdMeanArray, 'LineWidth', 1.0)
+hold on
+set(gca, 'YScale', 'log')
+set(gca, 'XScale', 'log')
+xlabel('Frequency (Hz)')
+ylabel('mag(Impedance) (Ohm)')
+
+yyaxis right
+errorbar( gamryStructure_EIS_cell_Day02(1).f, ...
+        meanPhaseArray, ...
+        stdPhaseArray, 'LineWidth', 1.0)
+hold on
+set(gca, 'XScale', 'log')
+xlabel('Frequency (Hz)')
+ylabel('Phase')
 
 % % CELL Day 3 % 
 % meanArray = [];
@@ -295,7 +318,7 @@ ylabel('Phase')
 % stdPhaseArray = std(PhaseArray, 0, 2);
 % 
 % yyaxis left
-% errorbar( gamryStructure_EIS_cell_Day02(1).f, ...
+% errorbar( gamryStructure_EIS_cell_Day03(1).f, ...
 %         meanMeanArray, ...
 %         stdMeanArray, 'LineWidth', 2.0)
 % hold on
@@ -330,14 +353,14 @@ for ii = 1:numMeasures
     PhaseArray = [PhaseArray gamryStructure_EIS_cell_Day01(jj).Phase];
 end
 
-% % 01/20 
-% pointerArray = [1 2]; 
-% numMeasures = length(pointerArray);
-% for ii = 1:numMeasures
-%     jj = pointerArray(ii);
-%     meanArray = [meanArray gamryStructure_EIS_cell_Day02(jj).Zmag];
-%     PhaseArray = [PhaseArray gamryStructure_EIS_cell_Day02(jj).Phase];
-% end
+% 01/20 
+pointerArray = [1 2]; 
+numMeasures = length(pointerArray);
+for ii = 1:numMeasures
+    jj = pointerArray(ii);
+    meanArray = [meanArray gamryStructure_EIS_cell_Day02(jj).Zmag];
+    PhaseArray = [PhaseArray gamryStructure_EIS_cell_Day02(jj).Phase];
+end
 % 
 % % 01/21
 % pointerArray = [1 2]; 
@@ -357,7 +380,7 @@ stdPhaseArray = std(PhaseArray, 0, 2);
 yyaxis left
 errorbar( gamryStructure_EIS_cell_Day01(1).f, ...
         meanMeanArray, ...
-        stdMeanArray, 'LineWidth', 2.0)
+        stdMeanArray, 'LineWidth', 1.0)
 hold on
 set(gca, 'YScale', 'log')
 set(gca, 'XScale', 'log')
@@ -368,11 +391,11 @@ xlim([10 1e6])
 yyaxis right
 errorbar( gamryStructure_EIS_cell_Day01(1).f, ...
         meanPhaseArray, ...
-        stdPhaseArray, 'LineWidth', 2.0)
+        stdPhaseArray, 'LineWidth', 1.0)
 hold on
 set(gca, 'XScale', 'log')
 xlabel('Frequency (Hz)')
 ylabel('Phase')
 xlim([9.9 1e6])
-legend('Cells Day 01', 'CM Day 01')
+legend('Cells Day 01', 'Cells Day 02', 'CM')
 
